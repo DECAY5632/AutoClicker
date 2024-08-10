bf709899a471e04e91d15e1a25cb9b4206144841
DECAY5632 <yhj01024345632@gmail.com>
2024-08-10 20:57:34 +0900

    check current on/off status

--- AutoClick.py
+++ AutoClick.py
@@ -5,6 +5,9 @@ import keyboard
 
 clicking = False
 
+def update_label():
+    label.config(text=f"현재 상태: {clicking} F5: 실행, F6: 중단")
+
 def click(): 
     while clicking:
         pyautogui.sleep(0.01)
@@ -13,11 +16,13 @@ def click():
 def start():
     global clicking
     clicking = True
+    update_label()
     threading.Thread(target=click).start()
 
 def stop():
     global clicking
     clicking = False
+    update_label()
 
 keyboard.add_hotkey('f5', start)
 keyboard.add_hotkey('f6', stop)
@@ -25,7 +30,7 @@ keyboard.add_hotkey('f6', stop)
 root = tk.Tk()
 root.title("AutoClick")
 
-label = tk.Label(root, text="F5: 실행, F6: 중단")
+label = tk.Label(root, text=f"현재 상태: {clicking} F5: 실행, F6: 중단")
 label.pack(pady=20)
 
 root.mainloop()
